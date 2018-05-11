e701be8f3decba84e005bc0f74a73f146b0a0ad7
Igor Busquets <igor.busquets@altran.com>
2018-05-11 10:46:20 +0100

    Altered DB with ON DELETE CASCADE Constraint

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -5,13 +5,13 @@ CREATE DATABASE StudentsCoursesDBfirst;
 USE StudentsCoursesDBfirst;
 
 CREATE TABLE Student (
-    ID int IDENTITY(1,1) PRIMARY KEY,
+    ID int IDENTITY(1,1) PRIMARY KEY on delete cascade,
 	FirstName varchar(30) NOT NULL,
     LastName varchar(30) NOT NULL
 );
 
 CREATE TABLE Course (
-    ID int IDENTITY(1,1) PRIMARY KEY,
+    ID int IDENTITY(1,1) PRIMARY KEY on delete cascade,
 	Name varchar(50) NOT NULL,
 	Vacancies int NOT NULL
 );
@@ -23,3 +23,14 @@ CREATE TABLE Registration(
 	RegistrationKey char(8) NOT NULL
 );
 
+ALTER TABLE Registration
+ADD CONSTRAINT std
+	FOREIGN KEY (Student_ID)
+	REFERENCES Student (ID)
+	ON DELETE CASCADE
+
+ALTER TABLE Registration
+ADD CONSTRAINT crs
+	FOREIGN KEY (Course_ID)
+	REFERENCES Course (ID)
+	ON DELETE CASCADE
