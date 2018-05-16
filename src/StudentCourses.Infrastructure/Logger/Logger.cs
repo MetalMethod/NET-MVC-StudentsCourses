19b8fc77c9f18fc451907cf4ce2e34eadfd2a532
Igor Busquets <igor.busquets@altran.com>
2018-05-16 15:16:09 +0100

    Logger for Exceptions implemented and intercepting any error in Global.asax

---
+++ src/StudentCourses.Infrastructure/Logger/Logger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StudentCourses.Infrastructure.Logger
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+
+        }
+
+        public static void LogException(Exception exception)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("\r\nLog Entry : ");
+            stringBuilder.Append("Date: " + DateTime.Now.ToLongDateString());
+            stringBuilder.Append(" ### Time: " + DateTime.Now.ToLongTimeString());
+            stringBuilder.Append("\n######################################################################\n");
+            stringBuilder.Append("Source: \n");
+            stringBuilder.Append(exception.Source.ToString());
+            stringBuilder.Append("\n--------------------------------------------------------------\n");
+            stringBuilder.Append("Message: \n");
+            stringBuilder.Append(exception.Message.ToString());
+            stringBuilder.Append("\n--------------------------------------------------------------\n");
+            stringBuilder.Append("Stack trace: \n");
+            stringBuilder.Append(exception.StackTrace.ToString());
+            stringBuilder.Append("\n######################################################################\r\n\n\n");
+
+            //string p = @"..\..\..\logs\";
+            string path = "C:\\Users\\Altran\\Desktop\\Igor\\personal_interests\\NET-MVC-StudentsCourses\\logs\\";
+            try
+            {
+                File.AppendAllText(path + "exceptions_log.txt", stringBuilder.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+            finally
+            {
+                stringBuilder.Clear();
+            }
+        }
+
+        public static void Log(string logMessage)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("\r\nLog Entry : ");
+            stringBuilder.Append("Date: " + DateTime.Now.ToLongDateString());
+            stringBuilder.Append(" ### Time: " + DateTime.Now.ToLongTimeString());
+            stringBuilder.Append("\n--------------------------------------------------------------\n");
+            stringBuilder.Append(logMessage);
+            stringBuilder.Append("\n--------------------------------------------------------------\n");
+            stringBuilder.Append("\n######################################################################\r\n\n\n");
+            string path = "C:\\Users\\Altran\\Desktop\\Igor\\personal_interests\\NET-MVC-StudentsCourses\\logs\\";
+            try
+            {
+                File.AppendAllText(path + "log.txt", stringBuilder.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+            }
+            finally
+            {
+                stringBuilder.Clear();
+            }
+        }
+    }
+}
--- src/StudentCourses.MVC/Global.asax.cs
+++ src/StudentCourses.MVC/Global.asax.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using StudentCourses.Infrastructure.AutoMapper;
+using System.IO;
+using System.Text;
+using StudentCourses.Infrastructure.Logger;
 
 namespace StudentCourses.MVC
 {
@@ -18,8 +21,45 @@ namespace StudentCourses.MVC
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            ///AutoMapper static class in Infrastuctore initialization
-            //AutoMapperConfiguration.Configure();
         }
+
+        protected void Application_Error()
+        {
+            Exception exception = Server.GetLastError();
+            // Redirect to error page
+
+            Logger.LogException(exception);
+
+            //Log(exception.StackTrace.ToString(), writer);
+
+            // log to file 
+            //exception.StackTrace.ToString();
+        }
+
+        //public static void Log(string logMessage)
+        //{
+        //    StringBuilder stringBuilder = new StringBuilder();
+
+        //    stringBuilder.Append("\r\nLog Entry : ");
+        //    stringBuilder.Append(DateTime.Now.ToLongTimeString());
+        //    stringBuilder.Append("\r\n-------------------------------\r\n");
+        //    stringBuilder.Append(logMessage);
+        //    stringBuilder.Append("\r\n-------------------------------\r\n");
+
+        //    string path = "C:\\Users\\Altran\\Desktop\\Igor\\personal_interests\\NET-MVC-StudentsCourses\\src\\StudentCourses.MVC\\Logs\\";
+        //    try
+        //    {
+        //        File.AppendAllText(path + "log.txt", stringBuilder.ToString());
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message.ToString());
+        //    }
+        //    finally
+        //    {
+        //        stringBuilder.Clear();
+        //    }    
+        //}
+
     }
 }
